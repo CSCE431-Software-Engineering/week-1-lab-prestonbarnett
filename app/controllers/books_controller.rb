41e484c5262a9a92e519a762fcc58a3a07ea8568
prestonbarnett <prestonb@tamu.edu>
2024-01-20 23:39:32 -0600

    Add partials for Books, implement delete confirmation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: %i[ show edit update destroy ]
+  before_action :set_book, only: %i[ show edit update destroy delete ]
 
   # GET /books or /books.json
   def index
@@ -19,6 +19,10 @@ class BooksController < ApplicationController
   def edit
   end
 
+  # GET /books/1/delete
+  def delete
+  end
+
   # POST /books or /books.json
   def create
     @book = Book.new(book_params)
@@ -26,10 +30,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.save
         format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -39,10 +41,8 @@ class BooksController < ApplicationController
     respond_to do |format|
       if @book.update(book_params)
         format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -52,8 +52,7 @@ class BooksController < ApplicationController
     @book.destroy
 
     respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
+      format.html { redirect_to books_url, notice: "Book was successfully deleted." }
     end
   end
 
