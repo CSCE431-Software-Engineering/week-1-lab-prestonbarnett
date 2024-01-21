0fb055ce45378b8653cd36cc4dd59ad91261386f
prestonbarnett <prestonb@tamu.edu>
2024-01-20 19:23:47 -0600

    Add price and author fields, install Bootstrap for styling components

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_19_192044) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_20_062548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_19_192044) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
+    t.decimal "price"
   end
 
 end
