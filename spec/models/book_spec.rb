9a588b22a2414e4916f6242152a5a19919697d23
prestonbarnett <prestonb@tamu.edu>
2024-01-29 17:40:48 -0600

    Add new unit tests for additional fields

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe Book, type: :model do
     described_class.new(title: "Harry Potter",
                         price: 10,
                         author: "J.K. Rowling",
-                        published: "2010-10-10")
+                        published: "2010-10-10",
+                        publisher: "Scholastic",
+                        release_date: "2010-10-10",
+                        pages: 300)
   }
 
   it "is valid with valid attributes" do
@@ -14,6 +17,21 @@ RSpec.describe Book, type: :model do
 
   it "is not valid without a title" do
     subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a publisher" do
+    subject.publisher = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a release date" do
+    subject.release_date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without pages" do
+    subject.pages = nil
     expect(subject).to_not be_valid
   end
 end
